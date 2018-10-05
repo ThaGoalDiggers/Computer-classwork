bbcb95e02fbe695ffcaff813b63a0f76bcf001ac
TroyCarroll48 <43215938+TroyCarroll48@users.noreply.github.com>
2018-10-04 21:19:59 -0400

    Add files via upload
    troys work

---
+++ Case.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+
+public class Case {
+
+    //Trying to test
+
+    String model;
+    String manuFacturer;
+    String powerSupply;
+    Dimension dimension;
+
+    public void pressPowerButton(){
+        System.out.println("Power Button Pressed");
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManuFacturer() {
+        return manuFacturer;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setManuFacturer(String manuFacturer) {
+        this.manuFacturer = manuFacturer;
+    }
+
+    public void setPowerSupply(String powerSupply) {
+        this.powerSupply = powerSupply;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    @Override
+    public String toString() {
+        return "Case{" +
+                "model='" + model + '\'' +
+                ", manuFacturer='" + manuFacturer + '\'' +
+                ", powerSupply='" + powerSupply + '\'' +
+                ", dimension=" + dimension +
+                '}';
+    }
+}
