466f414ec9ecd421781941811c029602c37c7284
Zachjrivera <Zachjrivera@gmail.com>
2018-10-04 23:01:30 -0400

    Problem with XML file trying to fix but this should completecode

--- src/main/java/Monitor.java
+++ src/main/java/Monitor.java
@@ -3,12 +3,12 @@ public class Monitor {
     private String model;
     private int size;
     private String manufacturer;
-    private String nativeResolution;
+    private Resolution nativeResolution;
 
-    public Monitor(String model, int size, String manufacturer,String nativeResolution) {
+    public Monitor(String model, int size, String manufacturer,Resolution nativeResolution) {
         this.model = model;
         this.size = size;
-        this.nativeResolution = nativeResolution;
+        this.nativeResolution= nativeResolution;
         this.manufacturer = manufacturer;
     }
 
@@ -20,12 +20,15 @@ public class Monitor {
         return size;
     }
 
-    public String getNativeResolution() {
+    public Resolution getNativeResolution() {
         return nativeResolution;
     }
 
+    public String getManufacturer() {
+        return manufacturer;
+    }
 
-    public void drawPixelAt(int x,int y,String color){
+    public void drawPixelAt(int x, int y, String color){
         System.out.println("Drawing pixel at " + x + "," + y + " " + " in color " + color);
     }
 
