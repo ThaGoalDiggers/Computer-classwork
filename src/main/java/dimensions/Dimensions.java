dfdb8fb81363223a8fce8c7bf6e62e06b0037af3
lamarwitherspoon <lamarwitherspoon@yahoo.com>
2018-10-05 03:08:16 -0400

    first commie

---
+++ src/main/java/dimensions/Dimensions.java
@@ -0,0 +1,63 @@
+package dimensions;
+
+public class Dimensions {
+
+
+//    Dimensions CLASS
+//    Create the following properties/instance variables as well as getters
+//    Create a constructor with all the properties
+//    int width;
+//    int height;
+//    int depth;
+//    Finally Override the toString Method for the Dimensions Class
+
+
+    private int width;
+
+    private int height;
+
+    private int depth;
+
+
+
+    public  Dimensions(int width, int height, int depth){
+
+        this.width = width;
+
+        this.height = height;
+
+        this.depth = depth;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(){
+
+
+    }
+
+
+    public int getHeight(){
+
+        return height;
+    }
+
+    public int getDepth(){
+
+        return depth;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Dimensions{" +
+                "width=" + width +
+                ", height=" + height +
+                ", depth=" + depth +
+                '}';
+    }
+
+}
